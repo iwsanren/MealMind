0b1dd6bc66d0c63b4d63f4b980ae7ecb89ea3758
Zhaofang <h1020230042@qq.com>
2026-08-06 23:57:00 +0100

    chore: set up Maven and Spring Boot configuration

---
+++ backend/src/main/java/com/mealmind/MealMindApplication.java
@@ -0,0 +1,12 @@
+package com.mealmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealMindApplication.class, args);
+    }
+}
